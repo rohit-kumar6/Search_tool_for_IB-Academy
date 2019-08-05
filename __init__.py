825c7266b69f5c3b16f979e63ddccfa12314d43f
rohit-kumar6 <rohit.kumar41996@gmail.com>
2019-08-06 00:48:07 +0530

    Added timeout in query and layout file

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, escape, request, render_template, Response
-# from flask_mysqldb import MySQL
+from flask_mysqldb import MySQL
 from flask import redirect, url_for, jsonify
 
 def create_app(test_config=None):
@@ -10,25 +10,52 @@ def create_app(test_config=None):
     except OSError:
         pass
     
-    # app.config['MYSQL_HOST'] = '127.0.0.1'
-    # app.config['MYSQL_USER'] = 'root'
-    # app.config['MYSQL_PASSWORD'] = ''
-    # app.config['MYSQL_DB'] = 'todo'
-    # mysql = MySQL(app)
+    app.config['MYSQL_HOST'] = '127.0.0.1'
+    app.config['MYSQL_USER'] = 'root'
+    app.config['MYSQL_PASSWORD'] = ''
+    app.config['MYSQL_DB'] = 'ib'
+    mysql = MySQL(app)
     
     # MODEL
+    def fetch_query(query):
+        try:
+            cur = mysql.connection.cursor()
+            sql = " SELECT question_name FROM question_table WHERE question_name like %(query)s "
+            cur.execute(sql,{ 'query': query })
+            console.loh(sql)
+            
+            # sql = " SELECT question_name FROM question_table WHERE question_name LIKE '%(query)%'"
+            # cur.execute(sql,{ 'query': query })
+            
+            fetch_data = cur.fetchall()#fetchone()
+            print("query",fetch_data)  
+            cur.close()
+            return fetch_data
+        except:
+            return []
+
     
     # CONTROLLER
+    @app.route('/find',methods=["POST"])
+    def find():
+        query = request.form.get('query')
+        
+        fetch_list = fetch_query(query)  
+        return render_template('search.html', fetch_list = fetch_list)
+
+    # VIEW
     @app.route('/')
     @app.route('/home')
     def home():
-        return render_template('home.html')  
-    
-    @app.route('/find',methods=["POST"])
-    def find():
-        query = request.form.get('query')        
-        dummy_fetch_list = ["1st","2nd"]
-        return render_template('search.html', fetch_list = dummy_fetch_list )
+        return render_template('home.html')
+
+    @app.route('/add')
+    def add():
+        return render_template('add.html') 
+
+    @app.route('/notfound')
+    def notfound():
+        return render_template('404.html')  
 
     return app
 
