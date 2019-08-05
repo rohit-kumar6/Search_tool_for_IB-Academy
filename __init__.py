6b368ac333d7b66f3bd1f3cfaa44d1dd45f29033
rohit-kumar6 <rohit.kumar41996@gmail.com>
2019-08-06 02:15:17 +0530

    Added some more backend api and add page to insert data in db

--- __init__.py
+++ __init__.py
@@ -20,28 +20,45 @@ def create_app(test_config=None):
     def fetch_query(query):
         try:
             cur = mysql.connection.cursor()
-            sql = " SELECT question_name FROM question_table WHERE question_name like %(query)s "
-            cur.execute(sql,{ 'query': query })
-            console.loh(sql)
-            
-            # sql = " SELECT question_name FROM question_table WHERE question_name LIKE '%(query)%'"
-            # cur.execute(sql,{ 'query': query })
-            
+            sql = " SELECT question_name FROM question_table WHERE question_name like '%{}%' or question_content like '%{}%'".format(query,query)
+            cur.execute(sql)
             fetch_data = cur.fetchall()#fetchone()
-            print("query",fetch_data)  
             cur.close()
             return fetch_data
         except:
             return []
+    
+    def get_company_id():
+        return
+    
+    def get_question_id():
+        return
 
     
     # CONTROLLER
     @app.route('/find',methods=["POST"])
     def find():
         query = request.form.get('query')
-        
         fetch_list = fetch_query(query)  
         return render_template('search.html', fetch_list = fetch_list)
+    
+    @app.route('/add_data',methods=["POST"])
+    def add_data():
+        question_name = request.form.get('question_name')
+        question_tag = request.form.get('question_tag')
+        question_content = request.form.get('question_content')
+        company_name = request.form.get('company_name')
+        batch_id = request.form.get('batch_id')
+        question_type = request.form.get('question_type')
+        week_number = request.form.get('week_number')
+        question_topic = request.form.get('question_topic')
+        day_number = request.form.get('day_number')
+        question_status = 0 # by default
+        print("----------------------------------------------------",question_name)
+        #insert
+        return Response(status=200)
+
+        
 
     # VIEW
     @app.route('/')
@@ -55,7 +72,9 @@ def create_app(test_config=None):
 
     @app.route('/notfound')
     def notfound():
-        return render_template('404.html')  
+        return render_template('404.html')
+
+
 
     return app
 
