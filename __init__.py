5f54fa9f3fe490df0892406a207febde943ddd00
rohit-kumar6 <rohit.kumar41996@gmail.com>
2019-08-09 00:06:34 +0530

    Searching with already Added Filter

--- __init__.py
+++ __init__.py
@@ -17,14 +17,27 @@ global fetch_list
 def getdb():
     return mysql.connection.cursor()  # Always open connection
 
-def fetch_query(query):
+def get_user_id():  # Always user 1 is login by default
+    return 1
+
+def fetch_query(query,Questions,Tags,Topic,Week,Day,Status):
     cur = getdb()
-    sql= """ SELECT comb2.q_id,comb2.question_name,comb2.question_tag,comb2.company_name,comb1.question_status from
-     (SELECT u2.q_id,u2.question_status FROM users_in_batches u1 JOIN user_wise_question u2 ON u1.user_id = u2.user_id 
-     WHERE u1.user_id = 1) comb1 JOIN (SELECT q1.q_id,q1.question_name,q1.question_tag,com.company_name FROM 
-     ((SELECT * FROM question_table WHERE 1) q1 LEFT JOIN (SELECT c1.q_id,c2.company_name FROM company_wise_question 
-     c1 JOIN company_table c2 WHERE c1.company_id = c2.company_id) com ON q1.q_id = com.q_id) WHERE q1.question_name 
-     LIKE "%{}%" or q1.question_content Like "%{}%" or com.company_name like "%{}%") comb2 WHERE comb1.q_id = comb2.q_id""".format(query,query,query)
+    u_id = get_user_id()
+    sql= """ SELECT a.q_id,a.question_name,a.question_tag,a.company,a.question_status,b.question_topic,b.question_type,b.Week_number,b.day_number FROM 
+    (SELECT comb2.q_id,comb2.question_name,comb2.question_tag,GROUP_CONCAT(comb2.company_name ORDER BY comb2.company_name) as "company",comb1.question_status from 
+    (SELECT u2.q_id,u2.question_status FROM users_in_batches u1 JOIN user_wise_question u2 ON u1.user_id = u2.user_id WHERE u1.user_id = "{}") 
+    comb1 JOIN
+    (SELECT q1.q_id,q1.question_name,q1.question_tag,com.company_name FROM ((SELECT * FROM question_table WHERE 1) q1 LEFT JOIN (SELECT c1.q_id,c2.company_name FROM company_wise_question c1 JOIN company_table c2 WHERE c1.company_id = c2.company_id) 
+    com ON q1.q_id = com.q_id) WHERE q1.question_name LIKE "%{}%" or q1.question_content Like "%{}%" or com.company_name like "%{}%") comb2 
+    WHERE comb1.q_id = comb2.q_id and comb1.question_status LIKE "{}%" and comb2.question_tag LIKE "{}%"
+    GROUP BY comb2.q_id) a
+    JOIN
+    (SELECT ba.q_id,ba.question_topic,ba.question_type,ba.Week_number,ba.day_number FROM batch_wise_question ba
+    JOIN
+    (SELECT batch_id FROM users_in_batches WHERE user_id = "{}") ba1
+    ON ba.batch_id = ba1.batch_id) b
+    WHERE a.q_id = b.q_id and b.question_topic LIKE "{}%" and b.question_type LIKE "{}%" and b.Week_number LIKE "{}%" and b.day_number LIKE "{}%"
+    """.format(u_id,query,query,query,Status,Tags,u_id,Topic,Questions,Week,Day)
     cur.execute(sql)
     fetch_data = cur.fetchall()
     return fetch_data
@@ -34,8 +47,14 @@ def fetch_query(query):
 def find():
     global fetch_list
     query = request.form['query']
+    Questions = request.form['Questions']
+    Tags = request.form['Tags']
+    Topic = request.form['Topic']
+    Week = request.form['Week']
+    Day = request.form['Day']
+    Status = request.form['Status']
     if query :
-        fetch_list = fetch_query(query) 
+        fetch_list = fetch_query(query,Questions,Tags,Topic,Week,Day,Status) 
     else:
         fetch_list = [] 
     return render_template('search.html', fetch_list = fetch_list)
